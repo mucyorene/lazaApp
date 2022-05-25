6be0d585215d69d26d8234b73d98bdd1528611dd
mucyorene <renemucyomucici@gmail.com>
2022-05-25 16:15:50 +0200

    working on wishlist with providers

--- lib/Model/providers/Other_controllers.dart
+++ lib/Model/providers/Other_controllers.dart
@@ -1,3 +1,11 @@
-class ProviderController {
+import 'package:flutter/cupertino.dart';
+import 'package:laza/Model/Others/WishModel.dart';
 
+class ProviderController extends ChangeNotifier {
+  final List<WishModels> _wishList = [];
+
+  void addToWishList(WishModels wishModels) {
+    _wishList.add(wishModels);
+    notifyListeners();
+  }
 }
